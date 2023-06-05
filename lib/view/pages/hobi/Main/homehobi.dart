4b12242592864f63b36be905fffe6dab93fb55e4
diksha-sontakke <84328313+diksha-sontakke@users.noreply.github.com>
2023-06-05 23:39:17 +0530

    05/06/2023

---
+++ lib/view/pages/hobi/Main/homehobi.dart
@@ -0,0 +1,24 @@
+
+import 'package:bts_app_demo/constant/constant.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeHobi  extends StatelessWidget {
+  const HomeHobi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+   
+    return Scaffold(
+      backgroundColor: hobiBackgroudColor,
+
+     
+
+
+
+
+
+    );
+  }
+}
